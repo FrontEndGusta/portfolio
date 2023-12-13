6cb2d98b9b3eb028755241216b160ce7937a258f
Gustavo Silva De Lima <gustavo.lima@cea.com.br>
2023-12-12 23:33:46 -0300

    ajustado useApi

--- src/services/useApi.ts
+++ src/services/useApi.ts
@@ -4,20 +4,11 @@ import axios from "axios";
 
 const useApi = <T>(endpoint: string) => {
   const { data, error, isLoading } = useQuery<T>(endpoint, async () => {
-    // Simula um atraso de 5 segundos
-    await new Promise(resolve => setTimeout(resolve, 5000));
-
-    try {
-      const response = await axios.get<T>(
-        `${process.env.REACT_APP_API_BASEURL}${endpoint}`
-      );
-      console.log(response);
-      return response.data;
-    } catch (error) {
-      // Você pode tratar o erro aqui se necessário
-      console.error("Erro na requisição:", error);
-      throw error;
-    }
+    const response = await axios.get<T>(
+      `${process.env.REACT_APP_API_BASEURL}${endpoint}`
+    );
+    console.log(response);
+    return response.data;
   });
 
   return { data, error, isLoading };
